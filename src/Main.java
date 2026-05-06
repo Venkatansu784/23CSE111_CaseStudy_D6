b40e0bf75ca0452f064dc5e4c143761e8d5e5e49
Venkatansu784 <karthiksankisa10@gmail.com>
2026-05-07 00:02:49 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -19,75 +21,135 @@ public class Main {
             return;
         }
 
-        Customer customer = new Customer(1, u, "mail@gmail.com");
+        Customer customer = new Customer(1, u, u + "@gmail.com");
 
         List<Product> products = new ArrayList<>();
-        products.add(new Product(1, "Laptop", "Electronics", 70000));
-        products.add(new Product(2, "Phone", "Electronics", 30000));
-        products.add(new Product(3, "Shoes", "Fashion", 2000));
-        products.add(new Product(4, "Watch", "Fashion", 5000));
+
+        try {
+
+            BufferedReader br = new BufferedReader(new FileReader("products.txt"));
+
+            String line;
+
+            while ((line = br.readLine()) != null) {
+
+                String[] data = line.split(",");
+
+                int id = Integer.parseInt(data[0]);
+                String name = data[1];
+                String category = data[2];
+                double price = Double.parseDouble(data[3]);
+
+                products.add(new Product(id, name, category, price));
+            }
+
+            br.close();
+
+        } catch (Exception e) {
+
+            System.out.println("Error loading products file.");
+
+        }
 
         ProductFilterService pf = new ProductFilterService(products);
         Cart cart = new Cart(101);
         OrderService os = new OrderService();
 
         while (true) {
+
             System.out.println("\n1.View 2.Search 3.Filter 4.Add 5.Remove 6.Cart 7.Checkout 8.History 9.Exit");
 
             int ch = sc.nextInt();
 
             if (ch == 1) {
+
                 for (Product pdt : pf.fetchItems()) {
                     System.out.println(pdt.getDetails());
                 }
+
             }
 
             else if (ch == 2) {
+
                 System.out.print("Search: ");
                 String s = sc.next();
+
                 for (Product pdt : pf.searchByName(s)) {
                     System.out.println(pdt.getDetails());
                 }
+
             }
 
             else if (ch == 3) {
+
                 System.out.print("Category: ");
                 String c = sc.next();
+
                 for (Product pdt : pf.applyCategoryFilter(c)) {
                     System.out.println(pdt.getDetails());
                 }
+
             }
 
             else if (ch == 4) {
+
                 System.out.print("Product ID: ");
                 int id = sc.nextInt();
+
                 for (Product pdt : products) {
-                    if (pdt.productID == id) cart.addItem(pdt);
+
+                    if (pdt.productID == id) {
+                        cart.addItem(pdt);
+                    }
+
                 }
+
             }
 
             else if (ch == 5) {
+
                 System.out.print("Product ID: ");
                 int id = sc.nextInt();
+
                 for (Product pdt : products) {
-                    if (pdt.productID == id) cart.removeItem(pdt);
+
+                    if (pdt.productID == id) {
+                        cart.removeItem(pdt);
+                    }
+
                 }
+
             }
 
             else if (ch == 6) {
+
                 cart.showCart();
+
             }
 
             else if (ch == 7) {
+
                 os.processCheckout(cart, customer);
+
             }
 
             else if (ch == 8) {
+
                 customer.viewOrderHistory();
+
             }
 
             else if (ch == 9) {
+
+                System.out.println("Thank You");
                 break;
+
+            }
+
+            else {
+
+                System.out.println("Invalid Choice");
+
             }
         }
     }
