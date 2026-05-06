a7373ea84af39613a070d33513c8e7a0d6bbf27f
Venkatansu784 <karthiksankisa10@gmail.com>
2026-05-07 00:32:43 +0530

    Update OrderService.java

--- src/OrderService.java
+++ src/OrderService.java
@@ -1,14 +1,44 @@
 import java.io.*;
 
 public class OrderService {
-
     int transactionID = 1;
 
     public Order processCheckout(Cart cart, Customer customer) {
 
         Order o = new Order(transactionID++, customer.customerID, cart.itemsList);
 
-        o.generateBill();
+        double total = 0;
+
+        try {
+            FileWriter fw = new FileWriter("order_history.txt", true);
+
+            fw.write("===== ONLINE SHOPPING SYSTEM - ORDER REPORT =====\n\n");
+
+            fw.write("Order ID: " + o.orderID + "\n");
+            fw.write("Customer ID: " + customer.customerID + "\n");
+            fw.write("Customer Name: " + customer.username + "\n\n");
+
+            fw.write("Products Purchased:\n");
+
+            for (Product p : cart.itemsList) {
+                fw.write(p.name + " (" + p.category + ") - " + p.price + "\n");
+                total += p.price;
+            }
+
+            fw.write("\nTotal Amount: " + total + "\n\n");
+
+            fw.write("Order Status: Confirmed\n");
+            fw.write("Payment Status: Successful\n");
+
+            fw.write("\n===============================================\n\n");
+
+            fw.close();
+
+            System.out.println("Order saved to order_history.txt");
+
+        } catch (Exception e) {
+            System.out.println("Error saving order.");
+        }
 
         saveToHistory(customer, o);
 
@@ -16,33 +46,6 @@ public class OrderService {
     }
 
     public void saveToHistory(Customer c, Order o) {
-
         c.addOrder(o);
-
-        try {
-
-            FileWriter writer = new FileWriter("order_history.txt", true);
-
-            writer.write("----- NEW ORDER -----\n");
-
-            writer.write("Order ID: " + o.orderID + "\n");
-
-            for (Product p : o.items) {
-                writer.write(p.getDetails() + "\n");
-            }
-
-            writer.write("----------------------\n\n");
-
-            writer.close();
-
-            System.out.println("Order saved to file.");
-
-        }
-
-        catch (Exception e) {
-
-            System.out.println("Error saving order.");
-
-        }
     }
 }
